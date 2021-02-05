c6bdf0f1f4e92901f2e19126211b7647daa2ad3f
Joshua Ellis <67237931+SpicyTakis@users.noreply.github.com>
2021-02-05 13:36:32 -0500

    nodejs docker build tool (python version soon)

--- build.js
+++ build.js
@@ -1,5 +1,27 @@
 const fs = require('fs');
+const childProcess = require('child_process')
+const exec = childProcess.exec
 
 args = process.argv
 
 files = fs.readdirSync(args[0])
+
+dockerfiles = []
+
+builtImages = 0
+
+for (const i in files; i.endsWith('.dockerfile')) {
+    dockerfiles.push(args[0] + i)
+}
+
+for (const dockerfile in dockerfiles) {
+    let content = fs.readfileSync(dockerfile)
+    let firstlines = content.split('\n', 2)
+    let imageName = firstlines.replace("# name: ", "")
+    let tag = firstline.replace("# tag: ", "")
+    exec(`docker build -t ${dockerfile} -t ${imageName}:${tag}`)
+    exec(`docker push ${imageName}:${tag}`)
+    builtImages++
+}
+
+console.log(`Successfully built and pushed ${builtImages} images!`) 
